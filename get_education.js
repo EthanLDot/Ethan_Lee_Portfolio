3edcfff239805a8a656efa7cf1c1b3bee80d45f1
EthanLDot <ethanlee2337@gmail.com>
2024-04-26 16:34:20 -0700

    MVP Done
    Added scripts to retrieve other experiences; rewrote about me and added noscript

---
+++ get_education.js
@@ -0,0 +1,64 @@
+const url = 'https://pub-905e1930f62c4043a73e59e819502421.r2.dev/education.json';
+
+document.addEventListener('DOMContentLoaded', function() {
+// Fetch data from the URL
+    fetch(url)
+    .then(response => {
+        // Check if the response status is OK (200)
+        if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+        }
+        // Convert the response to JSON
+        return response.json();
+    })
+    .then(data => {
+        var templates = document.createElement('template');
+        // Handle the JSON data
+        console.log('Data fetched:', data);
+
+        // start getting html templates
+        fetch('../templates/templates.html')
+        .then(response => response.text())
+                .then(html => {
+                    templates.innerHTML = html;
+
+                    data.forEach(function(entry, idx) {
+                        console.log(entry);
+                        //get template container
+                        var templateContainer = templates.content.querySelector('#entry-template').cloneNode(true).content;
+
+                        //get html for education
+                        var templateText = templates.content.querySelector('#education-text').cloneNode(true).content;
+                        templateText.getElementById('education-name').innerHTML = `<a href=${entry.link}><strong>${entry.name}</strong></a>`;
+                        templateText.getElementById('location').innerHTML = `${entry.location}`; 
+                        templateText.getElementById('date').innerHTML = `${entry.startDate} - ${entry.endDate}`; 
+                        var classesList = templateText.getElementById('classes-list');
+                        entry.courses.forEach(function(course, idx) {
+                            var listItem = document.createElement('li');
+                            var dt = document.createElement('dt');
+                            var dd = document.createElement('dd');
+                            dt.appendChild(document.createTextNode(course.name));
+                            dd.appendChild(document.createTextNode(course.description));
+                            listItem.appendChild(dt);
+                            listItem.appendChild(dd);
+                            classesList.appendChild(listItem);
+                        });
+                        templateText.getElementById('description').innerText = entry.description;
+                        templateText.getElementById('takeaway').innerHTML = `<strong>Takeaways:</strong> ${entry.takeaway}`;
+                        templateContainer.querySelector('#entry-details').appendChild(templateText);
+
+                        //inserts the content into the actual site
+                        document.getElementById('experience').querySelector('div').appendChild(templateContainer);
+            
+                    })
+                })
+                .catch(error => {
+                    console.error('Error fetching template:', error);
+                    return null;
+                });
+    })
+    .catch(error => {
+        // Handle any errors
+        console.error('There was a problem with the fetch operation:', error);
+    });
+})
---
+++ get_projects.js
@@ -0,0 +1,62 @@
+const url = 'https://pub-905e1930f62c4043a73e59e819502421.r2.dev/project.json';
+
+document.addEventListener('DOMContentLoaded', function() {
+// Fetch data from the URL
+    fetch(url)
+    .then(response => {
+        // Check if the response status is OK (200)
+        if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+        }
+        // Convert the response to JSON
+        return response.json();
+    })
+    .then(data => {
+        var templates = document.createElement('template');
+        // Handle the JSON data
+        console.log('Data fetched:', data);
+
+        //start getting html templates
+        fetch('../templates/templates.html')
+        .then(response => response.text())
+                .then(html => {
+                    templates.innerHTML = html;
+
+                    data.forEach(function(entry, idx) {
+                        console.log(entry);
+                        //get template container
+                        var templateContainer = templates.content.querySelector('#entry-template').cloneNode(true).content;
+
+                        //get html for experiences only
+                        var templateText = templates.content.querySelector('#project-text').cloneNode(true).content;
+                        templateText.getElementById('project-name').innerHTML = `<a href=${entry.link}><strong>${entry.name}</strong></a>`;
+                        templateText.getElementById('date').innerHTML = `${entry.date}`; 
+                        var skillsList = templateText.getElementById('skills-list');
+                        entry.skills.forEach(function(skill, idx) {
+                            var listItem = document.createElement('li');
+                            listItem.appendChild(document.createTextNode(skill));
+                            skillsList.appendChild(listItem);
+                        });
+                        templateText.getElementById('workedOnBy').innerText += entry.workedOnBy;
+                        templateText.getElementById('description').innerText += entry.description;
+                        templateText.getElementById('learned').innerText += entry.learned;
+                        
+                        
+                        // // console.log(templateText.querySelector('details summary').innerHTML);
+                        templateContainer.querySelector('#entry-details').appendChild(templateText);
+
+                        //inserts the content into the actual site
+                        document.getElementById('experience').querySelector('div').appendChild(templateContainer);
+            
+                    })
+                })
+                .catch(error => {
+                    console.error('Error fetching template:', error);
+                    return null;
+                });
+    })
+    .catch(error => {
+        // Handle any errors
+        console.error('There was a problem with the fetch operation:', error);
+    });
+})
